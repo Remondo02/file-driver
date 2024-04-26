96040ebb4f0d42cd1eb0ec5a63a21a5cd368a2ed
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-04-26 09:51:53 +0900

    Convex Query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@
 
 import { Button } from "@/components/ui/button"
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs"
-import { useMutation } from "convex/react"
+import { useMutation, useQuery } from "convex/react"
 import { api } from "../../convex/_generated/api"
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles)
   const createFile = useMutation(api.files.createFile)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -19,7 +20,14 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
-      <Button onClick={() => createFile({ name: "Hello world" })}>Click Me</Button>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>
+      })}
+
+      <Button onClick={() => createFile({ name: "Hello world" })}>
+        Click Me
+      </Button>
     </main>
   )
 }
