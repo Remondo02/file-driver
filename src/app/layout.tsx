96adde53a8112a03a30e4819087b1054117e1a8a
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-04-27 15:36:27 +0900

    Fix Node pb and rename component Header to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import ConvexClientProvider from "./ConvexClientProvider"
-import { Header } from "./Header"
+import { Header } from "./header"
 import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] })
