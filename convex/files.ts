24b50e31f6266d699791b8dc1083aa443a72f2c0
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-04-29 08:32:50 +0900

    Favorites Page

--- convex/files.ts
+++ convex/files.ts
@@ -63,6 +63,7 @@ export const getFiles = query({
   args: {
     orgId: v.string(),
     query: v.optional(v.string()),
+    favorites: v.optional(v.boolean()),
   },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity()
@@ -81,7 +82,7 @@ export const getFiles = query({
       return []
     }
 
-    const files = await ctx.db
+    let files = await ctx.db
       .query("files")
       .withIndex("by_orgId", (q) => q.eq("orgId", args.orgId))
       .collect()
@@ -89,12 +90,36 @@ export const getFiles = query({
     const query = args.query
 
     if (query) {
-      return files.filter((file) =>
+      files = files.filter((file) =>
         file.name.toLowerCase().includes(query.toLowerCase())
       )
-    } else {
-      return files
     }
+
+    if (args.favorites) {
+      const user = await ctx.db
+        .query("users")
+        .withIndex("by_tokenIdentifier", (q) =>
+          q.eq("tokenIdentifier", identity.tokenIdentifier)
+        )
+        .first()
+
+      if (!user) {
+        return files
+      }
+
+      const favorites = await ctx.db
+        .query("favorites")
+        .withIndex("by_userId_orgId_fileId", (q) =>
+          q.eq("userId", user._id).eq("orgId", args.orgId)
+        )
+        .collect()
+
+      files = files.filter((file) =>
+        favorites.some((favorite) => favorite.fileId === file._id)
+      )
+    }
+
+    return files
   },
 })
 
--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+
+import { useQuery } from "convex/react"
 import { FileBrowser } from "../_components/file-browser"
+import { api } from "../../../../convex/_generated/api.js"
 
 export default function FavoritesPage() {
+
   return (
     <div>
-      <FileBrowser title="Your Favorites" />
+      <FileBrowser title="Your Favorites" favorites />
     </div>
   )
 }
