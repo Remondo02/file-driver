d527b72e1790058b35be54d0c343ad0b8aeb24ff
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-04-30 15:33:51 +0900

    View Toggle

--- src/app/dashboard/_components/file-browser.tsx
+++ src/app/dashboard/_components/file-browser.tsx
@@ -1,18 +1,16 @@
 "use client"
-
 import { useOrganization, useUser } from "@clerk/nextjs"
 import { useQuery } from "convex/react"
 import { api } from "../../../../convex/_generated/api"
 import { UploadButton } from "./upload-button"
 import { FileCard } from "./file-card"
 import Image from "next/image.js"
-import { FileIcon, Loader2, StarIcon } from "lucide-react"
+import { GridIcon, Loader2, RowsIcon } from "lucide-react"
 import { SearchBar } from "./search-bar"
 import { useState } from "react"
-import { Button } from "@/components/ui/button"
-import Link from "next/link.js"
 import { DataTable } from "./file-table"
 import { columns } from "./columns"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 function Placeholder() {
   return (
@@ -70,32 +68,44 @@ export function FileBrowser({
 
   return (
     <div>
-      {isLoading && (
-        <div className="flex flex-col gap-8 w-full items-center mt-24">
-          <Loader2 className="h-32 w-32 animate-spin text-primary" />
-          <div className="text-2xl text-primary">Loading your files...</div>
-        </div>
-      )}
+      <div className="flex justify-between items-center mb-8">
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <SearchBar query={query} setQuery={setQuery} />
+        <UploadButton />
+      </div>
 
-      {!isLoading && (
-        <>
-          <div className="flex justify-between items-center mb-8">
-            <h1 className="text-4xl font-bold">{title}</h1>
-            <SearchBar query={query} setQuery={setQuery} />
-            <UploadButton />
+      <Tabs defaultValue="grid">
+        <TabsList className="mb-4">
+          <TabsTrigger value="grid" className="flex gap-2 items-center">
+            <GridIcon />
+            Grid
+          </TabsTrigger>
+          <TabsTrigger value="table" className="flex gap-2 items-center">
+            {" "}
+            <RowsIcon />
+            Table
+          </TabsTrigger>
+        </TabsList>
+
+        {isLoading && (
+          <div className="flex flex-col gap-8 w-full items-center mt-24">
+            <Loader2 className="h-32 w-32 animate-spin text-primary" />
+            <div className="text-2xl text-primary">Loading your files...</div>
           </div>
-
-          {files.length === 0 && <Placeholder />}
-
-          <DataTable columns={columns} data={modifiedFiles} />
-
+        )}
+        <TabsContent value="grid">
           <div className="grid grid-cols-3 gap-4">
             {modifiedFiles?.map((file) => {
               return <FileCard key={file._id} file={file} />
             })}
           </div>
-        </>
-      )}
+        </TabsContent>
+        <TabsContent value="table">
+          <DataTable columns={columns} data={modifiedFiles} />
+        </TabsContent>
+      </Tabs>
+
+      {files?.length === 0 && <Placeholder />}
     </div>
   )
 }
