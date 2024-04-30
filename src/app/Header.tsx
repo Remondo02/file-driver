6b56fbf92807cf3d4868d4bde8fffc4aa3b96bda
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-04-30 16:50:38 +0900

    Landing Page

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button"
 import {
   OrganizationSwitcher,
   SignInButton,
+  SignedIn,
   SignedOut,
   UserButton,
 } from "@clerk/nextjs"
@@ -10,15 +11,18 @@ import Link from "next/link.js"
 
 export function Header() {
   return (
-    <div className="border-b py-4 bg-gray-50">
+    <div className=" relative z-10 border-b py-4 bg-gray-50">
       <div className="items-center container mx-auto justify-between flex">
         <Link href="/" className="flex gap-2 items-center text-xl">
           <Image src="/logo.png" width="40" height="40" alt="file dirve logo" />
           FileDrive
         </Link>
-        <Button variant="outline">
-          <Link href="/dashboard/files">Your Files</Link>
-        </Button>
+
+        <SignedIn>
+          <Button variant="outline">
+            <Link href="/dashboard/files">Your Files</Link>
+          </Button>
+        </SignedIn>
 
         <div className="flex gap-2">
           <OrganizationSwitcher />
